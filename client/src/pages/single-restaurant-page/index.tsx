2a700ff5751897d9c56b99d9c47c656fd9b86a79
JoanaMas <joana.mastianica@gmail.com>
2023-02-27 11:41:09 +0200

    15 - Responsive single page UI created

--- client/src/pages/single-restaurant-page/index.tsx
+++ client/src/pages/single-restaurant-page/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Box, styled } from '@mui/material';
+import {
+  Box, styled, Stack, Typography, Button, Rating,
+} from '@mui/material';
 import routes from 'navigation/routes';
 import { useParams, Navigate } from 'react-router-dom';
 import Img from 'components/ui/img';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import useRestaurant from 'hooks/useRestaurant';
+import OpenInBrowserIcon from '@mui/icons-material/OpenInBrowser';
+import RingVolumeIcon from '@mui/icons-material/RingVolume';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 // Swipers styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -25,10 +30,59 @@ const SingleRestaurantPage = () => {
   if (restaurant === undefined) return null;
 
   return (
-    <>
-      <Box component="pre">{JSON.stringify(restaurant, null, 4)}</Box>
+    <Box component="pre">
+      <Stack sx={{
+        width: 1, height: '700px', opacity: '0.9',
+      }}
+      >
+        <Box sx={{
+          position: 'relative', width: '100%', height: '100%', padding: '30px',
+        }}
+        >
+          <Img src={restaurant.images[0]} sx={{ width: '100%', height: '100%' }} />
+        </Box>
+        <Stack
+          spacing={2}
+          sx={{
+            position: 'absolute', top: '10rem', left: '5rem', color: '#FFFFFF', backgroundColor: '#9689b8', height: { xs: '400px', sm: '350px' }, width: { xs: '400px', sm: '500px', md: '700px' }, padding: '2rem',
+          }}
+        >
+          <Rating readOnly value={restaurant.rating} precision={0.5} />
+          <Typography variant="h1" sx={{ fontSize: { xs: '25pt', md: '30pt' } }}>{restaurant.name}</Typography>
+          <Typography variant="body1" sx={{ fontSize: { xs: '8pt', sm: '10pt', md: '13pt' } }}>{restaurant.title}</Typography>
+
+          <Stack direction="row" spacing={4} sx={{ paddingTop: '2rem' }}>
+            <Button
+              href={restaurant.website}
+              sx={{
+                width: '50px', height: '50px', padding: '1rem', color: '#FFFFFF', border: 3,
+              }}
+            >
+              <OpenInBrowserIcon />
+            </Button>
+
+            <Button
+              href={`tel:${restaurant.phone}`}
+              sx={{
+                width: '50px', height: '50px', padding: '1rem', color: '#FFFFFF', border: 3,
+              }}
+            >
+              <RingVolumeIcon />
+            </Button>
+
+            <Stack direction="row" spacing={1} alignItems="center" sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
+              <LocationOnIcon />
+              <Typography>{restaurant.location.address}</Typography>
+              <Typography>{restaurant.location.city}</Typography>
+
+            </Stack>
+          </Stack>
+
+        </Stack>
+      </Stack>
+
       <Box sx={{
-        width: '1000px', height: '400px', mx: 'auto', pt: '40px',
+        width: '100%', height: '400px', pt: '40px',
       }}
       >
         <StyledSwiper
@@ -37,7 +91,7 @@ const SingleRestaurantPage = () => {
             clickable: true,
           }}
           spaceBetween={0}
-          slidesPerView={2}
+          slidesPerView={1}
         >
           {
             restaurant?.images.map((image) => (
@@ -46,7 +100,7 @@ const SingleRestaurantPage = () => {
                   src={image}
                   alt=""
                   sx={{
-                    width: 1, height: 1, border: 1, borderWidth: '40px', borderColor: '#9689b8', boxSizing: 'border-box',
+                    width: { xs: '500px', sm: '600px', md: '100%' }, height: 1, border: 1, borderWidth: '40px', borderColor: '#9689b8', boxSizing: 'border-box',
                   }}
                 />
               </SwiperSlide>
@@ -54,7 +108,7 @@ const SingleRestaurantPage = () => {
           }
         </StyledSwiper>
       </Box>
-    </>
+    </Box>
   );
 };
 
